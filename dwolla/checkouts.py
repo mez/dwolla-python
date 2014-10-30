d0a9738364f229387a75d19b92d7ecc718b18289
David Stancu <dstancu@nyu.edu>
2014-10-30 14:20:26 -0400

    added checkouts, half complete. need to implement get complete and verify

---
+++ dwolla/checkouts.py
@@ -0,0 +1,92 @@
+'''
+      _               _ _
+   __| |_      _____ | | | __ _
+  / _` \ \ /\ / / _ \| | |/ _` |
+ | (_| |\ V  V / (_) | | | (_| |
+  \__,_| \_/\_/ \___/|_|_|\__,_|
+
+  An official requests based wrapper for the Dwolla API.
+
+  This file contains functionality for all checkouts related endpoints.
+'''
+
+import dwolla
+
+
+class Checkouts(dwolla.DwollaRest):
+    def __init__(self):
+        self.cart = False
+
+    def resetcart(self):
+        self.cart = False
+
+    def addtocart(self, name, desc, cost, quantity):
+        if not name:
+            raise Exception('addtocart() requires name parameter')
+        if not desc:
+            raise Exception('addtocart() requires desc parameter')
+        if not cost:
+            raise Exception('addtocart() requires cost parameter')
+        if not quantity:
+            raise Exception('addtocart() requires quantity parameter')
+
+        if not self.cart:
+            self.cart = []
+
+        self.cart.append({
+            'name': name,
+            'description': desc,
+            'price': cost,
+            'quantity': quantity
+        })
+
+    def create(self, purchaseorder, params=False):
+        if not purchaseorder:
+            raise Exception('create() requires purchaseorder parameter')
+        if type(purchaseorder) is dict:
+            if not purchaseorder['destinationId']:
+                raise Exception('purchaseorder has no destinationId key')
+            if not self.cart and not purchaseorder['total']:
+                raise Exception('purchaseorder has no total amount')
+        else:
+            raise Exception('create() requires purchaseorder to be of type dict')
+
+        p = {
+            'client_id': self.settings['client_id'],
+            'client_secret': self.settings['client_secret'],
+            'purchaseOrder': purchaseorder
+        }
+
+        if self.cart:
+            p['purchaseOrder']['total'] = 0
+            for item in self.cart:
+                p['purchaseOrder']['total'] += (item['price'] * item['quantity'])
+            p['purchaseOrder']['orderItems'] = self.cart
+
+        if not p['purchaseOrder']['total']:
+            for item in p['purchaseOrder']['orderItems']:
+                p['purchaseOrder']['total'] += (item['price'] * item['quantity'])
+
+        if 'tax' in p['purchaseOrder']:
+            p['purchaseOrder']['total'] += p['purchaseOrder']['tax']
+        if 'shipping' in p['purchaseOrder']:
+            p['purchaseOrder']['total'] += p['purchaseOrder']['shipping']
+
+        if 'discount' in p['purchaseOrder']:
+            if p['purchaseOrder']['discount'] > 0:
+                p['purchaseOrder']['total'] -= p['purchaseOrder']['discount']
+            else:
+                p['purchaseOrder']['total'] += p['purchaseOrder']['discount']
+
+        p['purchaseOrder']['total'] = "{0:.2f}".format(p['purchaseOrder']['total'])
+
+        if params:
+            p = p.items + params.items
+
+        id = self._post('/offsitegateway/checkouts/', p)
+
+        if 'CheckoutId' in id:
+            return {'URL': self.settings['host'] + 'payment/checkout/' + id['CheckoutId']}.items + id.items
+        else:
+            raise Exception('Unable to create checkout due to API error.')
+
