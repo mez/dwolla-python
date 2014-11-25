f90d075c58de72dc1cf90e80668ac7f19a0de196
David Stancu <dstancu@nyu.edu>
2014-11-25 12:45:12 -0500

    finished oauth tests

--- tests/testOauth.py
+++ tests/testOauth.py
@@ -15,5 +15,13 @@ class OAuthTest(unittest.TestCase):
     def testgenauthurl(self):
         self.assertEqual(oauth.genauthurl(), 'https://uat.dwolla.com/oauth/v2/authenticate?client_id=SOME ID&response_type=code&scope=Balance|AccountInfo')
 
+    def testget(self):
+        oauth.get('CODE')
+        rest.r._post.assert_any_call('/token/', {'code': 'CODE', 'client_secret': 'SOME ID', 'grant_type': 'authorization_code', 'client_id': 'SOME ID'}, '/oauth/v2', False)
+
+    def testrefresh(self):
+        oauth.refresh('REFRESH')
+        rest.r._post.assert_any_call('/token/', {'client_secret': 'SOME ID', 'grant_type': 'refresh_token', 'refresh_token': 'REFRESH', 'client_id': 'SOME ID'}, '/oauth/v2', False)
+
 if __name__ == '__main__':
     unittest.main()
