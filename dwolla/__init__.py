89b27f4a4f5b2a2a41e28744a98f3ed1148dbb48
David Stancu <dstancu@nyu.edu>
2014-12-22 14:59:29 -0500

    version bump

--- dwolla/__init__.py
+++ dwolla/__init__.py
@@ -13,7 +13,7 @@
   Author -- Dwolla (David Stancu): api@dwolla.com, david@dwolla.com
   Copyright -- Copyright (C) 2014 Dwolla Inc.
   License -- MIT
-  Version -- 2.0.0
+  Version -- 2.0.1
   Link -- http://developers.dwolla.com
 '''
 
