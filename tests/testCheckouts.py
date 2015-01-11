a78bcdd837fc9311e8b8d2238d0ef76b1a06bf64
Mez <mez@jetcode.io>
2015-01-11 15:04:15 -0500

    fixed test to pass, although I dont think this is a very useful test

--- tests/testCheckouts.py
+++ tests/testCheckouts.py
@@ -8,7 +8,7 @@ class CheckoutsTest(unittest.TestCase):
         checkouts.r._get = MagicMock()
         checkouts.r._post = MagicMock()
 
-        checkouts.r._post.return_value = dict({'Response': {'CheckoutId': 'TEST'}})
+        checkouts.r._post.return_value = dict({'CheckoutId': 'TEST'})
 
         constants.client_id = "SOME ID"
         constants.client_secret = "SOME ID"
@@ -32,7 +32,7 @@ class CheckoutsTest(unittest.TestCase):
                 'key1': 'something',
                 'key2': 'another thing'
             })})
-        checkouts.r._post.assert_any_call('/offsitegateway/checkouts/', {'client_secret': 'SOME ID', 'purchaseOrder': {'destinationId': '812-740-4294', 'total': 15.0, 'notes': 'blahhh', 'orderItems': set([frozenset(['price', 'description', 'name', 'quantity'])]), 'metadata': frozenset(['key2', 'key1'])}, 'client_id': 'SOME ID'})
+        checkouts.r._post.assert_any_call('/offsitegateway/checkouts', {'client_secret': 'SOME ID', 'purchaseOrder': {'destinationId': '812-740-4294', 'total': 15.0, 'notes': 'blahhh', 'orderItems': set([frozenset(['price', 'description', 'name', 'quantity'])]), 'metadata': frozenset(['key2', 'key1'])}, 'client_id': 'SOME ID'})
 
     def testget(self):
         checkouts.get('123456')
